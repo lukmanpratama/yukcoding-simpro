65657970ea015a555f70e89b0de6bda316c11f01
lukmanpratama~ <zoezta01@gmail.com>
2023-10-23 23:33:32 +0700

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>Atlantis Lite - Bootstrap 4 Admin Dashboard</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="{{ asset('dashboard/assets/img/icon.ico') }}" type="image/x-icon"/>
+	<link rel="icon" href="{{ asset('dashboard/assets/img/yukcoding.ico') }}" type="image/x-icon"/>
 
 	<!-- Fonts and icons -->
 	<script src="{{ asset('dashboard/assets/js/plugin/webfont/webfont.min.js') }}"></script>
